1f66eac8b7e702fcbf21270b506d775450bcf429
yusayalcin <yusayalcin6@gmail.com>
2021-11-24 14:54:43 +0100

    profile and edit profile

--- src/shop/forms.py
+++ src/shop/forms.py
@@ -1,1 +1,19 @@
 from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
+from django.db import models
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username')
+
+class UpdateProfileForm(forms.ModelForm):
+    #avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+    facebook = forms.CharField(widget=forms.TextInput)
+    phone = forms.CharField(widget=forms.TextInput)
+
+    class Meta:
+        model = Profile
+        fields = ('bio', 'facebook', 'phone')
