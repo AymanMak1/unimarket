c263123f9898cb9ef88f6d22009f7c5387866b65
iberdiev <iskender.berdiev@gmail.com>
2021-12-02 17:48:58 +0100

    minor changes

--- src/shop/forms.py
+++ src/shop/forms.py
@@ -3,17 +3,21 @@ from django.contrib.auth.models import User
 from .models import Profile
 from django.db import models
 
+
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'username')
+        fields = ("first_name", "last_name", "email", "username")
+
 
 class UpdateProfileForm(forms.ModelForm):
-    #avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
-    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+    # avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    bio = forms.CharField(
+        widget=forms.Textarea(attrs={"class": "form-control", "rows": 5})
+    )
     facebook = forms.CharField(widget=forms.TextInput)
     phone = forms.CharField(widget=forms.TextInput)
 
     class Meta:
         model = Profile
-        fields = ('bio', 'facebook', 'phone')
+        fields = ("bio", "facebook", "phone")
